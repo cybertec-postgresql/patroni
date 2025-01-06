a0ab28ec7d4c9c8da42692ae3e73931f1f263653
András Váczi <andras.vaczi@cybertec.at>
2025-01-06 15:22:42 +0100

    Make target_site really optional

--- patroni/dcs/__init__.py
+++ patroni/dcs/__init__.py
@@ -464,7 +464,7 @@ class Failover(NamedTuple):
     leader: Optional[str]
     candidate: Optional[str]
     scheduled_at: Optional[datetime.datetime]
-    target_site: Optional[str]
+    target_site: Optional[str] = None
 
     @staticmethod
     def from_node(version: _Version, value: Union[str, Dict[str, str]]) -> 'Failover':
