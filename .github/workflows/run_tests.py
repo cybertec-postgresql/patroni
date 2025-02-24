69e8a91dfc7cb437cb53acdac281f9a5e8300ee7
avandras <andras.vaczi@cybertec.at>
2025-02-24 21:24:14 +0100

    Debug temp location 7

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -12,8 +12,8 @@ def main():
         flake8 = subprocess.call([sys.executable, 'setup.py', 'flake8'])
         test = subprocess.call([sys.executable, 'setup.py', 'test'])
         version = '.'.join(map(str, sys.version_info[:2]))
-        print(os.listdir(os.environ.get('GITHUB_WORKSPACE')))
-        # print(shutil.move('.coverage', os.path.join(tmp, '.coverage.' + version)))
+        # print(os.listdir(os.environ.get('GITHUB_WORKSPACE')))
+        print(shutil.move('.coverage', 'coverage'))
         # print(os.listdir(tmp))
         return flake8 | test
     elif what == 'combine':
