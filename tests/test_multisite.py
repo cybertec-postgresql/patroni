09084072dfe208af5b8024d132c9f68afd74fafa
avandras <andras.vaczi@cybertec.at>
2025-07-01 17:07:32 +0200

    Add further tests

--- tests/test_multisite.py
+++ tests/test_multisite.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import sys
@@ -12,12 +13,15 @@ from unittest.mock import MagicMock, Mock, mock_open, patch, PropertyMock
 from patroni import config, dcs
 from patroni import multisite
 from patroni.multisite import MultisiteController
-from patroni.dcs import Member
+from patroni.dcs import Cluster, ClusterConfig, Failover, Leader, Member, Status, SyncState, TimelineHistory
 from patroni.dcs.etcd import AbstractEtcdClientWithFailover
+from patroni.postgresql.misc import PostgresqlRole
 
 from tests.test_ha import Config
 from .test_etcd import etcd_read, etcd_write, socket_getaddrinfo
 from .test_ha import get_cluster_initialized_with_leader
+
+SYSID = '12345678901'
 
 # def get_cluster_config():
 #     return Config({"name": "", "scope": "alpha", "namespace": "batman", "loop_wait": 10, "ttl": 30, "retry_timeout": 10,
@@ -63,16 +67,43 @@ def get_member_with_wrong_data():
     with patch.dict(STANDBY_CONFIG, c):
         return Member.from_node(1, 'other_dc', 1, json.dumps(c))
 
+def get_cluster(initialize, leader, members, failover, cluster_config=None):
+    t = datetime.datetime.now().isoformat()
+    history = TimelineHistory(1, '[[1,67197376,"no recovery target specified","' + t + '","foo"]]',
+                              [(1, 67197376, 'no recovery target specified', t, 'foo')])
+    cluster_config = cluster_config or ClusterConfig(1, {'check_timeline': True, 'member_slots_ttl': 0}, 1)
+    s = SyncState.empty()
+    return Cluster(initialize, cluster_config, leader, Status(10, None, []), members, failover, s, history, False)
+
+def get_cluster_initialized_without_leader(leader=False, failover=None, cluster_config=None, failsafe=False):
+    m1 = Member(0, 'leader', 28, {'conn_url': 'postgres://replicator:rep-pass@127.0.0.1:5435/postgres',
+                                  'api_url': 'http://127.0.0.1:8008/patroni', 'xlog_location': 4,
+                                  'role': PostgresqlRole.PRIMARY, 'state': 'running'})
+    leader = Leader(0, 0, m1 if leader else Member(0, '', 28, {}))
+    m2 = Member(0, 'other', 28, {'conn_url': 'postgres://replicator:rep-pass@127.0.0.1:5436/postgres',
+                                 'api_url': 'http://127.0.0.1:8011/patroni',
+                                 'state': 'running',
+                                 'pause': True,
+                                 'tags': {'clonefrom': True},
+                                 'scheduled_restart': {'schedule': "2100-01-01 10:53:07.560445+00:00",
+                                                       'postgres_version': '99.0.0'}})
+    failsafe = {m.name: m.api_url for m in (m1, m2)} if failsafe else None
+    return get_cluster(SYSID, leader, [m1, m2], failover, cluster_config)
+
+
+def get_cluster_initialized_with_leader(failover=None):
+    return get_cluster_initialized_without_leader(leader=True, failover=failover)
+
 # def get_clusterleader():
     
 
-@patch.object(etcd.Client, 'write', etcd_write)
+# @patch.object(etcd.Client, 'write', etcd_write)
 # @patch.object(etcd.Client, 'read', etcd_read)
-@patch.object(etcd.Client, 'delete', Mock(side_effect=etcd.EtcdException))
+# @patch.object(etcd.Client, 'delete', Mock(side_effect=etcd.EtcdException))
 class TestMultisite(unittest.TestCase):
 
-    @patch('patroni.dcs.dcs_modules', Mock(return_value=['patroni.dcs.etcd']))
-    @patch.object(etcd.Client, 'read', etcd_read)
+    # @patch('patroni.dcs.dcs_modules', Mock(return_value=['patroni.dcs.etcd']))
+    # @patch.object(etcd.Client, 'read', etcd_read)
     def setUp(self):
         super(TestMultisite, self).setUp()
         os.environ[Config.PATRONI_CONFIG_VARIABLE] = PATRONI_CONFIG
@@ -184,7 +215,6 @@ class TestMultisite(unittest.TestCase):
             self.assertEqual(r, True)
 
     def test_check_transition(self):
-        # with patch.object(MultisiteController, 'on_change', Mock()) as o:
         on_change = Mock()
         os.environ[Config.PATRONI_CONFIG_VARIABLE] = PATRONI_CONFIG.replace('name: mstest', 'name: leader')
         self.config = Config(None)
@@ -194,6 +224,40 @@ class TestMultisite(unittest.TestCase):
         self.m._check_transition(True, 'blahblah')
         self.m.on_change.assert_called_once()
         self.assertEqual(self.m._has_leader, True)
+
+        # TODO: state_handler case
+
+    def test_resolve_multisite_leader(self):
+        pass
+
+    def test_observe_leader(self):
+        pass
+
+    def test_update_history(self):
+        pass
+
+    def test_check_for_failover(self):
+        self.multisite.dcs.manual_failover = Mock()
+
+        c = get_cluster_initialized_with_leader(Failover(0, '', 'foo', None, 'mstest'))
+        self.multisite._check_for_failover(c)
+        self.multisite.dcs.manual_failover.assert_called_with('', '')
+
+        c = get_cluster_initialized_with_leader(failover=None)
+        self.multisite._check_for_failover(c)
+        self.assertIsNone(self.multisite._failover_target)
+        self.assertIsNone(self.multisite._failover_timeout)
+
+        c = get_cluster_initialized_with_leader(Failover(0, '', 'foo', None, 'other'))
+        self.multisite._check_for_failover(c)
+        self.assertIsNotNone(self.multisite._failover_timeout)
+        self.assertEqual(self.multisite._failover_target, 'other')
+
+    def test_touch_member(self):
+        self.multisite.dcs.touch_member = Mock()
+        self.multisite.touch_member()
+        self.multisite.dcs.touch_member.assert_called_with({'host': '127.0.0.1', 'port': 5432})
+
 
     # def test_get_dcs_config_exception(self):
     #     print(self.config.local_configuration)
